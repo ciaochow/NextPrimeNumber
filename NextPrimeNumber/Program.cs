f3fe1311db0609bfb040a3eba1e1e4b6c61631f0
Ciao Chow <ciaochow15@zoho.com>
2015-09-16 19:15:54 -0400

    Initial Commit

---
+++ NextPrimeNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextPrimeNumber
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("----- Find Next Prime Number ------");
+            Console.WriteLine();
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+            double parsedinput = double.Parse(input);
+            double number = parsedinput;
+            while (true)
+            {
+                bool isPrime = true;
+                number += 1;
+                double squarenumber = Math.Sqrt(parsedinput);
+                for (int i = 2; i <= squarenumber; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine("The next prime number after {0} is {1}.", parsedinput, number);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
